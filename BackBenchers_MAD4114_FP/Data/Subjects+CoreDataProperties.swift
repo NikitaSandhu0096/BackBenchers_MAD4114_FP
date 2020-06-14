74a16bf3bd4ef2eb354b50ca7103f94c93a0959a
Kashyap Jhaveri <jhaverikashyap1998@gmail.com>
2020-06-14 12:15:32 -0400

    [Kashyap Jhaveri] Added coreData subClasses files

---
+++ BackBenchers_MAD4114_FP/Data/Subjects+CoreDataProperties.swift
@@ -0,0 +1,40 @@
+//
+//  Subjects+CoreDataProperties.swift
+//  BackBenchers_MAD4114_FP
+//
+//  Created by Kashyap Jhaveri on 2020-06-14.
+//  Copyright © 2020 Nikita Sandhu. All rights reserved.
+//
+//
+
+import Foundation
+import CoreData
+
+
+extension Subjects {
+
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<Subjects> {
+        return NSFetchRequest<Subjects>(entityName: "Subject")
+    }
+
+    @NSManaged public var subjectName: String?
+    @NSManaged public var notes: NSSet?
+
+}
+
+// MARK: Generated accessors for notes
+extension Subjects {
+
+    @objc(addNotesObject:)
+    @NSManaged public func addToNotes(_ value: Notes)
+
+    @objc(removeNotesObject:)
+    @NSManaged public func removeFromNotes(_ value: Notes)
+
+    @objc(addNotes:)
+    @NSManaged public func addToNotes(_ values: NSSet)
+
+    @objc(removeNotes:)
+    @NSManaged public func removeFromNotes(_ values: NSSet)
+
+}
