65b3a5ec6a9a0b09d13b74b102aa37a4c6d62b9e
Kashyap Jhaveri <jhaverikashyap1998@gmail.com>
2020-06-13 21:59:46 -0400

    [Kashyap Jhaveri] Added support for core data in the project

---
+++ Subjects+CoreDataClass.swift
@@ -0,0 +1,16 @@
+//
+//  Subjects+CoreDataClass.swift
+//  BackBenchers_MAD4114_FP
+//
+//  Created by Kashyap Jhaveri on 2020-06-13.
+//  Copyright © 2020 Nikita Sandhu. All rights reserved.
+//
+//
+
+import Foundation
+import CoreData
+
+
+public class Subjects: NSManagedObject {
+
+}
