c906bdb6a21308a32125706164bdc8f9afae84eb
Kashyap Jhaveri <jhaverikashyap1998@gmail.com>
2020-06-14 16:11:26 -0400

    [Kashyap Jhaveri] Added utility methods in Subjects class for insert, delete and fetch data

--- BackBenchers_MAD4114_FP/Data/Subjects+CoreDataClass.swift
+++ BackBenchers_MAD4114_FP/Data/Subjects+CoreDataClass.swift
@@ -10,7 +10,29 @@
 import Foundation
 import CoreData
 
-
 public class Subjects: NSManagedObject {
-
+    
+    static func fetchData() -> [Subjects]? {
+        let dataManager = AppDelegate.getDelegate().persistentContainer.viewContext
+        do {
+            if let result = try dataManager.fetch(Subjects.fetchRequest()) as? [Subjects]{
+                return result
+            }
+        } catch  {
+            print ("Error retrieving data")
+        }
+        return nil
+    }
+    
+    
+    static func addSubject(subjectName:String){
+        let dataManager = AppDelegate.getDelegate().persistentContainer.viewContext
+        let newEntity = NSEntityDescription.insertNewObject(forEntityName: "Subject", into: dataManager) as? Subjects
+        newEntity?.subjectName = subjectName
+    }
+    
+    static func deleteSubject(s:Subjects){
+        let dataManager = AppDelegate.getDelegate().persistentContainer.viewContext
+        dataManager.delete(s)
+    }
 }
